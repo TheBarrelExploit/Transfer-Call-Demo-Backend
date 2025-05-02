93c2b3bd76c30f6fd516ed170457bef8e212bbab
TheBarrelExploit <dramos2137@gmail.com>
2025-05-02 15:33:31 -0500

    Adicion de nuevos campos

--- src/shared/config.py
+++ src/shared/config.py
@@ -5,15 +5,20 @@ class Settings(BaseSettings):
     """settings for the application"""
     # General settings
     APP_NAME: str = "FastAPI Transfer Call"
+    PORT: int = 8000
+    DEBUG: bool = True
+    ALLOWED_HOSTS: list[str] 
 
     # Database settings
     MONGO_URI: str 
     MONGO_DB: str 
-
+    
+    # jwt settings
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    
     """Settings .env file"""
     model_config =  SettingsConfigDict(env_file=".env",case_sensitive=True)
-
-
 
 @lru_cache        
 def get_settings() -> Settings:
