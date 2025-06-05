0cb09f348aea3ebb48b6d604308860b5d31eb89b
pmorales21 <moralespaola888@gmail.com>
2025-06-05 18:24:06 -0500

    derechos de autor datasend

--- src/datasend/application/services.py
+++ src/datasend/application/services.py
@@ -1,3 +1,7 @@
+# Autores: Denuar Andres Ramos Lezama, Paola Andrea Morales Rodríguez
+# Fecha: Junio 2025
+# Proyecto: Demo Tarificador
+# Derechos reservados
 from datetime import timedelta
 from fastapi import BackgroundTasks
 from src.auth.infrastructure.security import create_access_token
