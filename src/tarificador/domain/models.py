fd32d879386f1c06065bfaf6f6fe82e0dc9e1e25
TheBarrelExploit <dramos2137@gmail.com>
2025-06-05 19:06:09 -0500

    Cambios models tarificador

--- src/tarificador/domain/models.py
+++ src/tarificador/domain/models.py
@@ -2,7 +2,8 @@
 # Autores: Denuar Andres Ramos Lezama, Paola Andrea Morales Rodríguez
 # Fecha: Junio 2025
 # Proyecto: Demo Tarificador
-# Derechos reservadosfrom dataclasses import dataclass, field
+# Derechos reservados
+from dataclasses import dataclass, field
 from typing import Optional, Any
 from enum import Enum
 from bson import ObjectId
